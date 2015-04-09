a22b559f61586eb0ca882ea81ad2a0e2164b5f6a
Anna Siewierska <absinthic@gmail.com>
2015-04-09 23:16:05 +0200

    Class has been improved.

--- src/hello.java
+++ src/hello.java
@@ -1,9 +1,13 @@
 
 public class hello {
+	
+	/**
+	 * Implement the application
+	 * @param args arguments
+	 */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello World");
 	}
 
 }
